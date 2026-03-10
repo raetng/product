c9ac98ed110e0190c7be389f9966d18ff09de007
raetng <raetng@uchicago.edu>
2026-03-10 02:17:32 -0500

    save

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
+const client = require('prom-client');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,8 +14,31 @@ const pool = new Pool({
   port: 5432,
 });
 
+// Prometheus metrics
+client.collectDefaultMetrics();
+const httpRequestDuration = new client.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Duration of HTTP requests in seconds',
+  labelNames: ['method', 'route', 'status_code'],
+  buckets: [0.005, 0.01, 0.05, 0.1, 0.5, 1, 5],
+});
+
 app.use(cors());
 app.use(express.json());
+
+// Request duration middleware
+app.use((req, res, next) => {
+  const end = httpRequestDuration.startTimer();
+  res.on('finish', () => {
+    end({ method: req.method, route: req.route?.path || req.path, status_code: res.statusCode });
+  });
+  next();
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
 
 app.get('/health', (req, res) => res.json({ status: 'OK' }));
 
